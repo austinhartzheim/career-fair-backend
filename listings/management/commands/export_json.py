898f026efeaa666e9b2e59ad8ffba41090bdb437
Austin Hartzheim <inbox@austinhartzheim.me>
2017-01-03 23:18:30 +0000

    Added the export_json command to the listing app.

---
+++ listings/management/commands/export_json.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+
+from django.core.management.base import BaseCommand
+from listings.models import Company
+
+
+class Command(BaseCommand):
+    help = 'Export company information to a JSON file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('outfile', type=argparse.FileType('w'))
+        parser.add_argument('--compact', '-c', action='store_true',
+                            help='Do not indent the JSON output')
+
+    def handle(self, **options):
+        companies = {}
+
+        # Load all companies
+        for company in Company.objects.all():
+            companies[company.pk] = company.get_object()
+
+        # Set up JSON export
+        json_kwargs = {
+            'indent': 2
+        }
+        if options['compact']:
+            json_kwargs['indent'] = 0
+
+        # Dump JSON to file
+        json.dump(companies, options['outfile'], **json_kwargs)
